ec1908ad95f2a918e6e4a9822fdf185858b3b9d9
nachoz12341 <40530732+nachoz12341@users.noreply.github.com>
2019-10-25 21:41:32 -0500

    added post route

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
         res.send("Hello daniel!");
 });
 
+app.post('/', (req,res) => {
+        const data = req.body.data;
+        res.send(data);
+});
+
 const port = process.env.PORT || 1337;
 app.listen(port);
 
