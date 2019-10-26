b2e52d1795a3aaca55f0d268db21367369a4af20
nachoz12341 <40530732+nachoz12341@users.noreply.github.com>
2019-10-26 05:28:45 -0500

    Added log messages!

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ app.get('/', (req, res) => {
         res.send("Hello daniel!");
 });
 
-app.post('/', (req,res) => {
-        const data = req.body.data;
-        res.send(data);
+app.post('/sensorData', (req,res) => {
+        const type = req.body.type;
+        const message = req.body.message;
+
+        console.log(type);
+        console.log(message);  
+        res.sendStatus(200);      
 });
 
 const port = process.env.PORT || 1337;
